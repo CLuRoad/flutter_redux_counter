6d7e2054e6bdb166483cbdc13a0a39083e1ff195
caolu <caolu@kuaishou.com>
2019-08-30 18:22:33 +0800

    用flutter_redux实现fluuter官方的计数器demo

--- lib/main.dart
+++ lib/main.dart
@@ -5,158 +5,87 @@ import 'package:built_value/built_value.dart';
 
 part 'main.g.dart';
 
+// App入口
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   // 创建store
   final store = new Store(
-      reducerBuilder.build(),
-      new Counter(),
-      new CounterActions()
+      reducerBuilder.build(),     ///< Reducer
+      new Counter(),              ///< State
+      new CounterActions()        ///< Actions
   );
-//  MyApp();
-  runApp(new ConnectionExample(store));
-}
-
-//class MyApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: ThemeData(
-//
-//        primarySwatch: Colors.blue,
-//      ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page'),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  int _counter = 0;
-//
-//  void _incrementCounter() {
-//    setState(() {
-//      _counter++;
-//    });
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
-//}
-
-/// an example using `StoreConnection`
-class ConnectionExample extends StatelessWidget {
-  final Store<Counter, CounterBuilder, CounterActions> store;
-
-  ConnectionExample(this.store);
-
-  @override
-  Widget build(BuildContext context) => new MaterialApp(
-    title: 'flutter_built_redux_test',
-    home: new ReduxProvider(
-      store: store,
-      child: new StoreConnection<Counter, CounterActions, int>(
-        connect: (state) => state.count,
-        builder: (BuildContext context, int count, CounterActions actions) {
-          return new Scaffold(
-            body: new Row(
-              children: <Widget>[
-                new RaisedButton(
-                  onPressed: () => actions.increment(5),
-                  child: new Text('Increment'),
-                ),
-                new Text('Count: $count'),
-              ],
-            ),
-          );
-        },
-      ),
-    ),
-  );
-}
-
-/// an example using a widget that implements `StoreConnector`
-class ConnectorExample extends StatelessWidget {
-  final Store<Counter, CounterBuilder, CounterActions> store;
-
-  ConnectorExample(this.store);
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'flutter_built_redux_test',
-      home: new ReduxProvider(
-        store: store,
-        child: new CounterWidget(),
+    return ReduxProvider(         ///< 放在App入口处，确保所有子Widget可以访问redux state
+      store: store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter_Redux Demo'),
       ),
     );
   }
 }
 
-/// [CounterWidget] impelments [StoreConnector] manually
-class CounterWidget extends StoreConnector<Counter, CounterActions, int> {
-  CounterWidget();
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  int connect(Counter state) => state.count;
-
-  @override
-  Widget build(BuildContext context, int count, CounterActions actions) =>
-      new Scaffold(
-        body: new Row(
-          children: <Widget>[
-            new RaisedButton(
-              onPressed: () {
-                actions.increment(1);
-              },
-              child: new Text('Increment'),
-            ),
-            new Text('Count: $count'),
-          ],
-        ),
-      );
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnection<Counter, CounterActions, Counter>(       ///< 监听state变化，rebuild子widget
+      connect: (state) => state,
+      builder: (BuildContext context, Counter counter, CounterActions actions){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'The current count is:',
+                ),
+                Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => actions.increment(5),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
+    );
+  }
+}
+
+// [ReducerBuilder]可以理解为 管理action和其Imp的容器
 ReducerBuilder<Counter, CounterBuilder> reducerBuilder = new ReducerBuilder()
 ..add(CounterActionsNames.increment, (s, a, b) {
   int count = s.count + a.payload;
   b..count = count;
 });
 
+// Actions 在这里添加action
 abstract class CounterActions extends ReduxActions {
   factory CounterActions() => new _$CounterActions();
   CounterActions._();
@@ -164,6 +93,7 @@ abstract class CounterActions extends ReduxActions {
   ActionDispatcher<int> increment;
 }
 
+// State
 abstract class Counter implements Built<Counter, CounterBuilder> {
   factory Counter() => new _$Counter._(count: 0);
   Counter._();
